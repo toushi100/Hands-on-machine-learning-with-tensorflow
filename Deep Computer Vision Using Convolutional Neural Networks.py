d63adcf9c9169c07d54e96e29a70d8df90a17e2e
toushi100 <toushi2009@gmail.com>
2020-09-30 15:37:22 +0200

    chapter 14 completed

---
+++ Deep Computer Vision Using Convolutional Neural Networks.py
@@ -0,0 +1,331 @@
+import sys
+import tensorflow_datasets as tfds
+from sklearn.datasets import load_sample_image
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import os
+from functools import partial
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+np.random.seed(42)
+tf.random.set_seed(42)
+
+PROJECT_DIR = ""
+CHAPTER_ID = "cnn"
+IMAGES_PATH = os.path.join(PROJECT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+
+def plot_image(image):
+    plt.imshow(image, cmap="gray", interpolation="nearest")
+    plt.axis("off")
+
+
+def plot_color_image(image):
+    plt.imshow(image, interpolation="nearest")
+    plt.axis("off")
+
+
+china = load_sample_image("china.jpg") / 255
+flower = load_sample_image("flower.jpg") / 255
+
+images = np.array([china, flower])
+batch_size, height, width, channel = images.shape
+
+filters = np.zeros(shape=(7, 7, channel, 2), dtype=np.float32)
+filters[:, 3, :, 0] = 1
+filters[3, :, :, 1] = 1
+outputs = tf.nn.conv2d(images, filters, strides=1, padding="SAME")
+
+plt.imshow(outputs[0, :, :, 1], cmap="gray")
+plt.axis("off")
+plt.show()
+
+for image_index in (0, 1):
+    for feature_map in (0, 1):
+        plt.subplot(2, 2, image_index * 2 + feature_map + 1)
+        plot_image(outputs[image_index, :, :, feature_map])
+plt.show()
+
+
+def crop(images):
+    return images[150:220, 130: 250]
+
+
+plot_image(crop(images[0, :, :, 0]))
+save_fig("china_original", tight_layout=False)
+plt.show()
+
+for feature_map, filename in enumerate(["china_vertical", "china_horizontal"]):
+    plot_image(crop(outputs[0, :, :, feature_map]))
+    save_fig(filename, tight_layout=False)
+    plt.show()
+
+# implementing ResNet-34
+
+DefaultConv2D = partial(keras.layers.Conv2D, kernel_size=3, strides=1, padding="SAME", use_bias=False)
+
+
+class ResdualUnit(keras.layers.Layer):
+    def __init__(self, filters, strides=1, activation="relu", **kwargs):
+        super().__init__(**kwargs)
+        self.activation = keras.activations.get(activation)
+        self.main_layers = [
+            DefaultConv2D(filters, strides=strides),
+            keras.layers.BatchNormalization(),
+            self.activation,
+            DefaultConv2D(filters),
+            keras.layers.BatchNormalization()
+        ]
+        self.skip_layers = []
+        if strides > 1:
+            self.skip_layers = [
+                DefaultConv2D(filters, kernel_size=1, strides=strides),
+                keras.layers.BatchNormalization()
+            ]
+
+    def call(self, inputs):
+        z = inputs
+        for layer in self.main_layers:
+            z = layer(z)
+        skip_z = inputs
+        for layer in self.skip_layers:
+            skip_z = layer(skip_z)
+        return self.activation(z + skip_z)
+
+
+model = keras.models.Sequential()
+model.add(DefaultConv2D(64, kernel_size=7, strides=2, input_shape=[28, 28, 1]))
+model.add(keras.layers.BatchNormalization())
+model.add(keras.layers.Activation("relu"))
+model.add(keras.layers.MaxPool2D(pool_size=3, strides=2, padding="SAME"))
+prev_filters = 64
+for filters in [64] * 3 + [128] * 4 + [256] * 6 + [512] * 3:
+    strides = 1 if filters == prev_filters else 2
+    model.add(ResdualUnit(filters, strides=strides))
+    prev_filters = filters
+model.add(keras.layers.GlobalAveragePooling2D())
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(10, activation="softmax"))
+model.summary()
+
+(X_train_full, y_train_full), (X_test, y_test) = keras.datasets.fashion_mnist.load_data()
+X_train, X_valid = X_train_full[:-5000], X_train_full[-5000:]
+y_train, y_valid = y_train_full[:-5000], y_train_full[-5000:]
+
+X_mean = X_train.mean(axis=0, keepdims=True)
+X_std = X_train.std(axis=0, keepdims=True) + 1e-7
+X_train = (X_train - X_mean) / X_std
+X_valid = (X_valid - X_mean) / X_std
+X_test = (X_test - X_mean) / X_std
+
+X_train = X_train[..., np.newaxis]
+X_valid = X_valid[..., np.newaxis]
+X_test = X_test[..., np.newaxis]
+
+model.compile(loss="sparse_categorical_crossentropy", optimizer="nadam", metrics=["accuracy"])
+history = model.fit(X_train, y_train, epochs=10, validation_data=(X_valid, y_valid))
+score = model.evaluate(X_test, y_test)
+X_new = X_test[:10]  # pretend we have new images
+y_pred = model.predict(X_new)
+
+# using a pretrained model
+
+model = keras.applications.resnet50.ResNet50(weights="imagenet")
+images_resized = tf.image.resize(images, [224, 224])
+plot_color_image(images_resized[0])
+plt.show()
+
+images_resized = tf.image.resize_with_pad(images, 224, 224, antialias=True)
+plot_color_image(images_resized[0])
+plt.show()
+
+images_resized = tf.image.resize_with_crop_or_pad(images, 224, 224)
+plot_color_image(images_resized[0])
+plt.show()
+
+china_box = [0, 0.03, 1, 0.68]
+flower_box = [0.19, 0.26, 0.86, 0.7]
+images_resized = tf.image.crop_and_resize(images, [china_box, flower_box], [0, 1], [224, 224])
+plot_color_image(images_resized[0])
+plt.show()
+plot_color_image(images_resized[1])
+plt.show()
+
+inputs = keras.applications.resnet50.preprocess_input(images_resized * 255)
+Y_proba = model.predict(inputs)
+print(Y_proba.shape)
+
+top_K = keras.applications.resnet50.decode_predictions(Y_proba, top=3)
+for image_index in range(len(images)):
+    print("Image #{}".format(image_index))
+    for class_id, name, y_proba in top_K[image_index]:
+        print(" {} -{:12s} {:.2f}".format(class_id, name, y_proba * 100))
+    print()
+
+dataset, info = tfds.load("tf_flowers", as_supervised=True, with_info=True)
+print(dataset)
+print(info.splits)
+print(info.even_splits
+      ["trian"])
+class_names = info.features["labels"].names
+print(class_names)
+
+n_classes = info.features["label"].num_classes
+dataset_size = info.splits["train"].num_classes
+print(dataset_size)
+
+test_set_raw, valid_set_raw, train_set_raw = tfds.load("tf_flowers",
+                                                       split=["train[:10%]", "train[10%:25%]", "train[25%:]"],
+                                                       as_supervised=True)
+plt.figure(figsize=(12, 10))
+index = 0
+for image, label in train_set_raw.take(9):
+    index += 1
+    plt.subplot(3, 3, index)
+    plt.imshow(image)
+    plt.title("class: {}".format(class_names[label]))
+    plt.axis("off")
+plt.show()
+
+
+def preprocessing(image, label):
+    resized_image = tf.image.resize(image, [224, 224])
+    final_image = keras.applications.xception.preprocess_input(resized_image)
+    return final_image, label
+
+
+def central_crop(image):
+    shape = tf.shape(image)
+    min_dim = tf.reduce_min([shape[0], shape[1]])
+    top_crop = (shape[0] - min_dim) // 4
+    bottom_crop = shape[0] - top_crop
+    left_crop = (shape[0] - min_dim) // 4
+    right_crop = shape[0] - left_crop
+    return image[top_crop:bottom_crop, left_crop:right_crop]
+
+
+def random_crop(image):
+    shape = tf.shape(image)
+    min_dim = tf.reduce_min([shape[0], shape[1]])
+    return tf.image.random_crop(image, [min_dim, min_dim, 3])
+
+
+def preprocess(image, label, randomize=False):
+    if randomize:
+        cropped_image = random_crop(image)
+        cropped_image = tf.image.random_flip_left_right(cropped_image)
+    else:
+        cropped_image = central_crop(image)
+    resized_image = tf.image.resize(cropped_image, [224, 224])
+    final_image = keras.applications.xception.preprocess_input(resized_image)
+    return final_image
+
+
+batch_size = 64
+train_set = train_set_raw.shuffle(1000).repeat()
+train_set = train_set.map(partial(preprocess, randomize=True)).batch(batch_size).prefetch(1)
+valid_set = train_set_raw.map(preprocess).batch(batch_size).prefetch(1)
+test_set = test_set_raw.map(preprocess).batch(batch_size).prefetch(1)
+
+plt.figure(figsize=(12, 12))
+for X_batch, y_batch in train_set.take(1):
+    for index in range(9):
+        plt.subplot(3, 3, index + 1)
+        plt.imshow(X_batch[index] / 2 + 0.5)
+        plt.title("Class: {}".format(class_names[y_batch[index]]))
+        plt.axis("off")
+
+plt.show()
+
+plt.figure(figsize=(12, 12))
+for X_batch, y_batch in test_set.take(1):
+    for index in range(9):
+        plt.subplot(3, 3, index + 1)
+        plt.imshow(X_batch[index] / 2 + 0.5)
+        plt.title("Class: {}".format(class_names[y_batch[index]]))
+        plt.axis("off")
+
+plt.show()
+
+base_model = keras.applications.xception.Xception(weights="imagenet", include_top=False)
+avg = keras.layers.GlobalAveragePooling2D()(base_model.output)
+output = keras.layers.Dense(n_classes, activation="softmax")(avg)
+model = keras.models.Model(inputs=base_model, outputs=output)
+
+for index, layer in enumerate(base_model.layers):
+    print(index.layer.name)
+
+for layer in base_model.layers:
+    layer.trainable = False
+    optimizer = keras.optimizers.SGD(lr=0.2, momentum=0.9, decay=0.01)
+    model.compile(loss="sparse_categorical_crossentropy", optimizer=optimizer, metrics=["accuracy"])
+    history = model.fit(train_set,
+                        steps_per_epoch=int(0.75 * dataset_size / batch_size),
+                        validation_data=valid_set,
+                        validation_steps=int(0.15 * dataset_size / batch_size),
+                        epochs=5)
+
+for layer in base_model:
+    layer.trainable = True
+    optimizer = keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True, decay=0.01)
+    model.compile(loss="sparse_categorical_entropy", optimizer=optimizer, metrics=["accuracy"])
+    history = model.fit(train_set,
+                        steps_per_epoch=int(0.75 * dataset_size / batch_size),
+                        validation_data=valid_set,
+                        validation_steps=int(0.15 * dataset_size / batch_size),
+                        epochs=45)
+
+# Classification and Localization
+
+base_model = keras.applications.xception.Xception(weights="imagenet", include_top=False)
+avg = keras.layers.GlobalAveragePooling2D()(base_model.output)
+class_output = keras.layers.Dense(n_classes, activation="softmax")(avg)
+loc_output = keras.layers.Dense(4)(avg)
+model = keras.models.Model(inputs=base_model.input, outputs=[class_output, loc_output])
+model.compile(loss=["sparse_categorical_crossentropy", "mse"],
+              loss_weights=[0.8, 0.2],
+              optimizer=optimizer, metrics=["accuracy"])
+
+
+def add_random_bounding_boxes(images, labels):
+    fake_bboxes = tf.random.uniform([tf.shape(image)[0], [4]])
+    return images, (labels, fake_bboxes)
+
+
+fake_train_set = train_set.take(5).repeat(2).map(add_random_bounding_boxes())
+
+model.fit(fake_train_set, steps_per_epoch=5, epochs=2)
+
+
+def maximum_precision(precision):
+    return np.flip(np.maximum.accumulate(np.flip(precision)))
+
+
+recalls = np.linespace(0, 1, 11)
+precisions = [0.91, 0.94, 0.96, 0.94, 0.95, 0.92, 0.80, 0.60, 0.45, 0.20, 0.10]
+max_precisions = maximum_precisions(precisions)
+mAP = max_precisions.mean()
+plt.plot(recalls, precisions, "ro--", label="Precision")
+plt.plot(recalls, max_precisions, "bo-", label="Max Precision")
+plt.xlabel("Recall")
+plt.ylabel("Precision")
+plt.plot([0, 1], [mAP, mAP], "g:", linewidth=3, label="mAP")
+plt.grid(True)
+plt.axis([0, 1, 0, 1])
+plt.legend(loc="lower center", fontsize=14)
+plt.show()
